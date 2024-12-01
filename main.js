f432a8129103b357f6459792061419863ecb4521
clacla17 <clafairy07@gmail.com>
2024-12-01 18:27:28 +0800

    liked songs playlist redirect

---
+++ main.js
@@ -0,0 +1,7 @@
+// to like playlist
+// Select the button by its ID
+const redirectButton = document.getElementById('likesongbttn');
+
+redirectButton.addEventListener("click", function() {
+    window.location.href = "likedplaylist.html"; 
+});
