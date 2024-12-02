41084f24f245003834651ebee0d6b5ffb8493d7c
clacla17 <clafairy07@gmail.com>
2024-12-03 01:37:57 +0800

    updated homepage with scrollable side panel

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // to like playlist
 // Select the button by its ID
-const redirectButton = document.getElementById('likesongbttn');
-
-redirectButton.addEventListener("click", function() {
-    window.location.href = "likedplaylist.html"; 
-});
+document.getElementById("likesongbttn").onclick = function() {
+    window.location.href = "likedplaylist.html";
+};
