72753826e51fe2350b7707a94ba4c26416b04b3f
clacla17 <clafairy07@gmail.com>
2024-12-11 23:19:02 +0800

    try to connect

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,12 +1,12 @@
 
 document.getElementById("likesongbttn").onclick = function() {
-    window.location.href = "/Html/likedplaylist.html";
+    window.location.href = "Html/likedplaylist.html";
 };
 
 document.getElementById("likesongbttn2").onclick = function() {
-    window.location.href = "/Html/likedplaylist.html";
+    window.location.href = "Html/likedplaylist.html";
 };
 
 document.getElementById("browsebttn").onclick = function() {
-    window.location.href = "/Html/browse.html";
+    window.location.href = "Html/browse.html";
 };
